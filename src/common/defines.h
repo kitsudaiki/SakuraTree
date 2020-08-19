d2dd2dbe8bf3ff7a03e5977d5591a083827ec518
Tobias Anker <tobias.anker@kitsunemimi.moe>
2020-08-19 19:45:15 +0200

    add logo at the beginning of a run

--- src/common/defines.h
+++ src/common/defines.h
@@ -25,4 +25,38 @@
 
 #include <common/includes.h>
 
+#define ASCII_LOGO "" \
+"                                                                        \n" \
+"                               ,o'    ,l'                               \n" \
+"                             ;OK0Kd..x0KKO;                             \n" \
+"                           .kKx' .xKKc  :OKx.                           \n" \
+"                          .OKd     '.     0KO.                          \n" \
+"                          kKXO,         .;KXKk                          \n" \
+"                         c0l;lkKxc.  'lOKx:;xK;                         \n" \
+"                         kc     ``KKX0´´     Ox                         \n" \
+"              ......     K.       ,XX.       c0     .....               \n" \
+"        .,lxO0Oxolll00dclK        ,XX.       `Kc:xKxllodkKKOdc'         \n" \
+"      'k00d´´       kX,           ,XX.           oX,      ``lOXKx.      \n" \
+"       c0k.        .0Xo.         .oXX;          .kXo.        ;KK:       \n" \
+"        lKO;.  .;d00ocxKOl'   .cOKkclOKx:.   ,d0KdcdKOl'   .oKXc        \n" \
+"    'okOo:ckKkkKk´´     ``0Kk00´´      ``K0kKk´´     ``OKx0Kd;ckKOo.    \n" \
+"    .OK0.   .KK.           lXd            0X'           :Xd    oKXO     \n" \
+"     .OKO.   KK            :Xo            0X.           ,Xo   lKKk.     \n" \
+"       l00c  KK            :Xo            OX.           ,Xo .xK0c       \n" \
+"        .lOOlKK'           oXx.          .0X:           cXkdKOc         \n" \
+"           'oOKK0o,    .:xKkokKkc.    ,o00do0Kd:.   .:xKKKkl.           \n" \
+"              .,:d0KkoO0´´     ``00dkKk´´     ``KOdOKKdc'.              \n" \
+"                'd..0Kc           cK0.           kXx cx.                \n" \
+"               :k.  OK,           ,KO            xXo  c0'               \n" \
+"              ;K'   OK,           ,KO            xXo   x0.              \n" \
+"             .0k    OK;           ;KO            kXd   ,Kk              \n" \
+"             cKd.;d0Ox0O:.     .:k0xx0x:.    .;d0koO0o,,KK'             \n" \
+"             dKK0d´´   ``k0dcd0xc.,ko.'lkOolk0o´´   ``d0KK:             \n" \
+"             lKK.  ..     .oKd  .dO',kc. .0K;      ..  lKK;             \n" \
+"             ,KKOk0KK.     :Kd;x0c    l0o,0K.     :KK0k0KK.             \n" \
+"              lc;'.dK;...':xKKOc       .cOKKo;'.. oKd.';c:              \n" \
+"                   lKKKKKKOd:.            .:dOKKKKKKc                   \n" \
+"                   .c;;,..                     .,;:c.                   \n" \
+"                                                                        \n"
+
 #endif // DEFINES_H
